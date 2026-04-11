9073002eef5e2d490b7ef4a4050c12604d1dd022
marcuszxc <marcuszxc10@gmail.com>
2026-04-11 12:31:50 +0200

    Added command to fix agent forwardning in catchy os

--- config.fish
+++ config.fish
@@ -2,3 +2,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 source ~/.config/fish/functions/update.fish
 
 abbr -a ll "ls -la"
+
+if not set -q SSH_AUTH_SOCK
+    set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+end
+
+set -gx SSH_ASKPASS /usr/bin/ksshaskpass
