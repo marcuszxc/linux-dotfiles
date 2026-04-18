712bc95ea0376fb82f71d3c5f216d24885059e5a
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2026-04-18 17:35:27 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-wget -O ~/.config/fish/functions/update.fish https://raw.githubusercontent.com/marcuszxc/linux-aliases/cachy-os/update.fish 
-wget -O ~/.config/fish/config.fish https://raw.githubusercontent.com/marcuszxc/linux-aliases/cachy-os/config.fish
-systemctl --user enable --now ssh-agent.socket
