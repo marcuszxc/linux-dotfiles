38baa1105014dedbee69d80a67acf46382172020
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2026-04-18 17:32:09 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+wget -O ~/.config/fish/functions/update.fish https://raw.githubusercontent.com/marcuszxc/linux-aliases/cachy-os/update.fish 
+wget -O ~/.config/fish/config.fish https://raw.githubusercontent.com/marcuszxc/linux-aliases/cachy-os/config.fish
+systemctl --user enable --now ssh-agent.socket
